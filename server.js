2835ce4a4eb4d232a701a75eb11baae5f81705df
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-06-05 13:47:57 -0700

    app and server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+require('./lib/utils/connect')();
+const app = require('./lib/app');
+
+const PORT = process.env.PORT || 7890;
+
+app.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
+});
