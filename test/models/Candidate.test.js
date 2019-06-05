f3c8daad2b3207a93e3c7a9dfdc88c30b97ccf1a
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-06-05 16:51:17 -0700

    2nd Candidate schema test passing

--- test/models/Candidate.test.js
+++ test/models/Candidate.test.js
@@ -15,4 +15,14 @@ describe('candidate model', () => {
       _id: expect.any(mongoose.Types.ObjectId)
     });
   });
+  it('has required name, bio, and image fields', () => {
+    const candidate2 = new Candidate({
+    
+    });
+    const errors = candidate2.validateSync().errors;
+    
+    expect(errors.name.message).toEqual('Path `name` is required.');
+    expect(errors.bio.message).toEqual('Path `bio` is required.');
+    expect(errors.image.message).toEqual('Path `image` is required.');
+  });
 });
