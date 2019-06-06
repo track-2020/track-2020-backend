5eb8339d025564fdbac70cb865b2c61acfb46032
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-06-05 17:25:31 -0700

    changed port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 require('./lib/utils/connect')();
 const app = require('./lib/app');
 
-const PORT = process.env.PORT || 7890;
+const PORT = process.env.PORT || 8888;
 
 app.listen(PORT, () => {
   console.log(`listening on ${PORT}`);
