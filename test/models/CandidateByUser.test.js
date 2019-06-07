da67f17dc7f923ade0bbc5dd180d02b05909bc9f
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-06-07 09:18:27 -0700

    added 'user' 2 candidatebyuser/revised issue #'s

--- test/models/CandidateByUser.test.js
+++ test/models/CandidateByUser.test.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
 const CandidateByUser = require('../../lib/models/CandidateByUser');
 
-describe('candidate model', () => {
-  it('has candidate and scores', () => {
+describe('candidateByUser model', () => {
+  it('has candidate, user, and scores', () => {
     const candidateOne = new CandidateByUser({
       candidate: mongoose.Types.ObjectId(),
+      user: mongoose.Types.ObjectId(),
+      issue0Score: {
+        issue: mongoose.Types.ObjectId(),
+        score: 0
+      },
       issue1Score: {
         issue: mongoose.Types.ObjectId(),
         score: 0
@@ -18,16 +23,17 @@ describe('candidate model', () => {
         score: 0
       },
       issue4Score: {
-        issue: mongoose.Types.ObjectId(),
-        score: 0
-      },
-      issue5Score: {
         issue: mongoose.Types.ObjectId(),
         score: 0
       }
     });
     expect(candidateOne.toJSON()).toEqual({
       candidate: expect.any(mongoose.Types.ObjectId),
+      user: expect.any(mongoose.Types.ObjectId),
+      issue0Score: {
+        issue: expect.any(mongoose.Types.ObjectId),
+        score: 0
+      },
       issue1Score: {
         issue: expect.any(mongoose.Types.ObjectId),
         score: 0
@@ -44,14 +50,10 @@ describe('candidate model', () => {
         issue: expect.any(mongoose.Types.ObjectId),
         score: 0
       },
-      issue5Score: {
-        issue: expect.any(mongoose.Types.ObjectId),
-        score: 0
-      },
       _id: expect.any(mongoose.Types.ObjectId)
     });
   });
-  it('has a required candidate field', () => {
+  it('has a required candidate and user field', () => {
     const candidateOne = new CandidateByUser({
       issue1Score: {
         issue: mongoose.Types.ObjectId(),
@@ -77,5 +79,6 @@ describe('candidate model', () => {
     const errors = candidateOne.validateSync().errors;
 
     expect(errors.candidate.message).toEqual('Path `candidate` is required.');
+    expect(errors.user.message).toEqual('Path `user` is required.');
   });
 });
