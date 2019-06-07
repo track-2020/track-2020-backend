e4916d482fb7c2e496146e686284a91a7e02916d
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-06-06 17:39:45 -0700

    find issues and find issue by id working

--- test/routes/issues.test.js
+++ test/routes/issues.test.js
@@ -5,6 +5,12 @@ const Issue = require('../../lib/models/Issue');
 const connect = require('../../lib/utils/connect');
 const mongoose = require('mongoose');
 
+const issueOne = {
+  title: 'I am issue',
+  description: 'I am important',
+  image: 'image.jpg'
+};
+
 describe('issues routes', () => {
   beforeAll(() => {
     return connect();
@@ -34,6 +40,26 @@ describe('issues routes', () => {
           __v: 0,
           _id: expect.any(String)
         });
+      });
+  });
+  it('finds all issues', () => {
+    return Issue.create(issueOne)
+      .then(() => {
+        return request(app)
+          .get('/api/v1/issues')
+          .then(foundIssues => {
+            expect(foundIssues.body).toHaveLength(1);
+          });
+      });
+  });
+  it('finds an issue by id', () => {
+    return Issue.create(issueOne)
+      .then(createdIssue => {
+        return request(app)
+          .get(`/api/v1/issues/${createdIssue._id}`)
+          .then(foundIssue => {
+            expect(foundIssue.body).toEqual({ ...issueOne, _id: expect.any(String) });
+          });
       });
   });
 });
